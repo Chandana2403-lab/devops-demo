0b846cde95085ea6fbb498d474217c1330420f57
Chandana2403-lab <chandanaj269@gmail.com>
2026-08-30 11:49:47 +0530

    fix: run app as persistent Flask server on port 5000

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hii everyone, welcome to the DevOps application!"
+    return "Hiiii everyone, welcome to the DevOps application!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
