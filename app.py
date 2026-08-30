a16fb3481cfeacb412eab172f390889db66fa8b0
Chandana2403-lab <chandanaj269@gmail.com>
2026-08-30 13:00:13 +0530

    ci/cd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hiiii everyone, welcome to the DevOps application!"
+    return "Hiiii everyone, welcome to the DevOps application(CI/CD)!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
